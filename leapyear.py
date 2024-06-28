288bdb3478d5c7df44783fb1c25d4138439b5570
rakeshmettu68 <rakeshmettu68@gmail.com>
2024-06-28 14:55:25 +0530

    add leap year program

---
+++ leapyear.py
@@ -0,0 +1,19 @@
+a = int(input("Enter a year: "))
+"""if a%4==0 and a%100 !=0:
+    print("Given year is a leap year")
+elif a%400==0:
+    print("Given year is a leap year")
+else:
+    print("Given year is not  a leap year")"""
+    
+if a%4==0:
+    if a%100==0:
+        if a%400==0:
+            print("YES Given year is leap year %d"%(a))   
+        else:
+            print("Sorry Given year %d is not leap year "%(a))
+    else:
+        print("Sorry Given year %d is not leap year "%(a))
+else:
+    print("Sorry Given year %d is not leap year "%(a))
+    
